ab7532022a4c14fd9ef5d1709de979c2c32a7c75
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-03-03 16:29:10 +0530

    Bird Rotation fixed

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -8,6 +8,7 @@ public class BirdMovement : MonoBehaviour
     // Start is called before the first frame update
     public float birdSpeed;
     Rigidbody2D rb;
+    public int angle, minAngle, maxAngle;
     void Start()
     {
        gameObject.AddComponent<Rigidbody2D>();
@@ -18,13 +19,32 @@ public class BirdMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             //Bird movement will start after left click mouse button
             BirdFlapJump();
             //transform.Translate(Vector2.right * 4 * Time.deltaTime);
-          
         }
+        BirdRotation();
+    }
+
+    private void BirdRotation()
+    {
+        if (rb.velocity.y > 0)
+        {
+            if (angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+        }
+        else if (rb.velocity.y < 0)
+        {
+            if (angle >= minAngle)
+            {
+                angle = angle - 4;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     private void BirdFlapJump()
